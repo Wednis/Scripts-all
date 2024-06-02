fb167ab2e45cac64e6930c15628f1b4e137dc40f
Wednis <150148936+Wednis@users.noreply.github.com>
2024-06-02 12:27:13 +0800

    Update 白名单异或获取字符.php

--- php/白名单异或获取字符.php
+++ php/白名单异或获取字符.php
@@ -1,15 +1,16 @@
-//取自19年国赛N0rths师傅写的，我优化了输出
+//取自19年国赛N0rths师傅写的，我进行了部分优化
 //注意，php不能异或类似/*，/1da21等的字符，目前不清楚原因
 <?php
 $whitelist = ['abs', 'acos', 'acosh', 'asin', 'asinh', 'atan2', 'atan', 'atanh', 'base_convert', 'bindec', 'ceil', 'cos', 'cosh', 'decbin', 'dechex', 'decoct', 'deg2rad', 'exp', 'expm1', 'floor', 'fmod', 'getrandmax', 'hexdec', 'hypot', 'is_finite', 'is_infinite', 'is_nan', 'lcg_value', 'log10', 'log1p', 'log', 'max', 'min', 'mt_getrandmax', 'mt_rand', 'mt_srand', 'octdec', 'pi', 'pow', 'rad2deg', 'rand', 'round', 'sin', 'sinh', 'sqrt', 'srand', 'tan', 'tanh'];
 $whitelist2 = [ 'acos', 'acosh', 'asin', 'asinh', 'atan2', 'atan', 'atanh', 'base_convert', 'bindec', 'ceil', 'cos', 'cosh', 'decbin', 'dechex', 'decoct', 'deg2rad', 'exp', 'expm1', 'floor', 'fmod', 'getrandmax', 'hexdec', 'hypot', 'is_finite', 'is_infinite', 'is_nan', 'lcg_value', 'log10', 'log1p', 'log', 'max', 'min', 'mt_getrandmax', 'mt_rand', 'mt_srand', 'octdec', 'pi', 'pow', 'rad2deg', 'rand', 'round', 'sin', 'sinh', 'sqrt', 'srand', 'tan', 'tanh','abs'];
+$str = " /";          //改这里即可
 
 foreach ($whitelist as $i):
     foreach ($whitelist2 as $k):
-    $match=$k^$i^" /";
+    $match=$k^$i^$str;
     if (!(preg_match("/[^0-9a-f]/",$match))){
-			echo $k."^".$i."^"." /"."=".$match;
-		    echo "\n";	
+	    echo $k."^".$i."^".$str."=".$match;
+	    echo "\n";	
 	}
     endforeach;
 endforeach;
